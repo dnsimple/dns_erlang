95ce28fcc7e18a63138b9d46facd2ce822b73784
Andrew Tunnell-Jones <andrew@tj.id.au>
2010-12-14 10:14:02 +0000

    Year should be in copyright notice

--- src/dns_record_info.erl
+++ src/dns_record_info.erl
@@ -1,6 +1,6 @@
 %% -------------------------------------------------------------------
 %%
-%% Copyright (c) Andrew Tunnell-Jones. All Rights Reserved.
+%% Copyright (c) 2010 Andrew Tunnell-Jones. All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
