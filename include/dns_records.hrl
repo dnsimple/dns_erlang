8f7ec958fdebc0a2030023b46069c725e6b52220
Simone Carletti <weppos@weppos.net>
2023-11-09 14:02:08 +0100

    rebar3 fmt

--- include/dns_records.hrl
+++ include/dns_records.hrl
@@ -2,34 +2,41 @@
 -define('__dns_records.hrl__', ok).
 -include("dns_terms.hrl").
 
--record(dns_message, {id = dns:random_id() :: dns:message_id(),
-		      qr = false :: 0..1 | boolean(),
-		      oc = ?DNS_OPCODE_QUERY :: dns:opcode(),
-		      aa = false :: 0..1 | boolean(),
-		      tc = false :: 0..1 | boolean(),
-		      rd = false :: 0..1 | boolean(),
-		      ra = false :: 0..1 | boolean(),
-		      ad = false :: 0..1 | boolean(),
-		      cd = false :: 0..1 | boolean(),
-		      rc = ?DNS_RCODE_NOERROR :: dns:rcode(),
-		      qc = 0 :: 0..65535,
-		      anc = 0 :: 0..65535,
-		      auc = 0 :: 0..65535,
-		      adc = 0 :: 0..65535,
-		      questions = [] :: dns:questions(),
-		      answers = [] :: dns:answers(),
-		      authority = [] :: dns:authority(),
-		      additional = [] :: dns:additional()}).
+-record(dns_message, {
+    id = dns:random_id() :: dns:message_id(),
+    qr = false :: 0..1 | boolean(),
+    oc = ?DNS_OPCODE_QUERY :: dns:opcode(),
+    aa = false :: 0..1 | boolean(),
+    tc = false :: 0..1 | boolean(),
+    rd = false :: 0..1 | boolean(),
+    ra = false :: 0..1 | boolean(),
+    ad = false :: 0..1 | boolean(),
+    cd = false :: 0..1 | boolean(),
+    rc = ?DNS_RCODE_NOERROR :: dns:rcode(),
+    qc = 0 :: 0..65535,
+    anc = 0 :: 0..65535,
+    auc = 0 :: 0..65535,
+    adc = 0 :: 0..65535,
+    questions = [] :: dns:questions(),
+    answers = [] :: dns:answers(),
+    authority = [] :: dns:authority(),
+    additional = [] :: dns:additional()
+}).
 
--record(dns_query, {name :: dns:dname(),
-		    class = ?DNS_CLASS_IN :: dns:class(),
-		    type :: dns:type()}).
+-record(dns_query, {
+    name :: dns:dname(),
+    class = ?DNS_CLASS_IN :: dns:class(),
+    type :: dns:type()
+}).
 
--record(dns_rr, {name :: dns:dname(),
-		 class = ?DNS_CLASS_IN :: dns:class(),
-		 type :: dns:type(),
-		 ttl = 0 :: dns:ttl(),
-		 data :: dns:rrdata()}).
+-record(dns_rr, {
+    name :: dns:dname(),
+    class = ?DNS_CLASS_IN :: dns:class(),
+    type :: dns:type(),
+    ttl = 0 :: dns:ttl(),
+    data :: dns:rrdata()
+}).
+
 -record(dns_rrdata_a, {ip}).
 -record(dns_rrdata_aaaa, {ip}).
 -record(dns_rrdata_afsdb, {subtype, hostname}).
@@ -45,10 +52,7 @@
 -record(dns_rrdata_cds, {keytag, alg, digest_type, digest}).
 -record(dns_rrdata_hinfo, {cpu, os}).
 -record(dns_rrdata_https, {svc_priority, target_name, svc_params}).
--record(dns_rrdata_ipseckey, {precedence,
-			      alg,
-			      gateway,
-			      public_key}).
+-record(dns_rrdata_ipseckey, {precedence, alg, gateway, public_key}).
 -record(dns_rrdata_key, {type, xt, name_type, sig, protocol, alg, public_key}).
 -record(dns_rrdata_kx, {preference, exchange}).
 -record(dns_rrdata_loc, {size, horiz, vert, lat, lon, alt}).
@@ -57,12 +61,7 @@
 -record(dns_rrdata_minfo, {rmailbx, emailbx}).
 -record(dns_rrdata_mr, {newname}).
 -record(dns_rrdata_mx, {preference, exchange}).
--record(dns_rrdata_naptr, {order,
-			   preference,
-			   flags,
-			   services,
-			   regexp,
-			   replacement}).
+-record(dns_rrdata_naptr, {order, preference, flags, services, regexp, replacement}).
 -record(dns_rrdata_ns, {dname}).
 -record(dns_rrdata_nsec, {next_dname, types}).
 -record(dns_rrdata_nsec3, {hash_alg, opt_out, iterations, salt, hash, types}).
@@ -70,23 +69,19 @@
 -record(dns_rrdata_nxt, {dname, types}).
 -record(dns_rrdata_ptr, {dname}).
 -record(dns_rrdata_rp, {mbox, txt}).
--record(dns_rrdata_rrsig, {type_covered,
-			   alg,
-			   labels,
-			   original_ttl,
-			   expiration,
-			   inception,
-			   key_tag,
-			   signers_name,
-			   signature}).
+-record(dns_rrdata_rrsig, {
+    type_covered,
+    alg,
+    labels,
+    original_ttl,
+    expiration,
+    inception,
+    key_tag,
+    signers_name,
+    signature
+}).
 -record(dns_rrdata_rt, {preference, host}).
--record(dns_rrdata_soa, {mname,
-			 rname,
-			 serial,
-			 refresh,
-			 retry,
-			 expire,
-			 minimum}).
+-record(dns_rrdata_soa, {mname, rname, serial, refresh, retry, expire, minimum}).
 -record(dns_rrdata_spf, {spf}).
 -record(dns_rrdata_srv, {priority, weight, port, target}).
 -record(dns_rrdata_sshfp, {alg, fp_type, fp}).
@@ -94,21 +89,18 @@
 -record(dns_rrdata_tsig, {alg, time, fudge, mac, msgid, err, other}).
 -record(dns_rrdata_txt, {txt}).
 
--record(dns_optrr, {udp_payload_size = 4096,
-		    ext_rcode = ?DNS_ERCODE_NOERROR,
-		    version = 0,
-		    dnssec = false,
-		    data = []}).
+-record(dns_optrr, {
+    udp_payload_size = 4096,
+    ext_rcode = ?DNS_ERCODE_NOERROR,
+    version = 0,
+    dnssec = false,
+    data = []
+}).
 -record(dns_opt_llq, {opcode, errorcode, id, leaselife}).
 -record(dns_opt_nsid, {data}).
 -record(dns_opt_owner, {seq = 0, primary_mac, wakeup_mac, password}).
 -record(dns_opt_ul, {lease}).
--record(dns_opt_ecs, {
-		      family,
-		      source_prefix_length, 
-		      scope_prefix_length,
-		      address
-		     }).
+-record(dns_opt_ecs, {family, source_prefix_length, address}).
 -record(dns_opt_unknown, {id, bin}).
 
 -endif.
--- src/dns_record.erl
+++ src/dns_record.erl
@@ -31,28 +31,29 @@ serialise(Tuple) when is_tuple(Tuple) -> serialise(Tuple, []).
 
 serialise(Term, Opts) when is_list(Opts) ->
     WrapFun = proplists:get_value(wrap_fun, Opts, fun(X) -> X end),
-    Term0 = case Term of
-		Data when is_binary(Data) -> bin_to_hex(Data);
-		Data when is_tuple(Data) ->
-		    [Tag|Values] = tuple_to_list(Data),
-		    Fields = dns_record_info:fields(Tag),
-		    Fun = fun(Field, Value) ->
-				  serialise(Tag, Field, Value, Opts)
-			  end,
-		    Proplist = lists:zipwith(Fun, Fields, Values),
-		    {atom_to_binary(Tag, utf8), Proplist};
-		_ -> erlang:error(badarg)
-	    end,
+    Term0 =
+        case Term of
+            Data when is_binary(Data) -> bin_to_hex(Data);
+            Data when is_tuple(Data) ->
+                [Tag | Values] = tuple_to_list(Data),
+                Fields = dns_record_info:fields(Tag),
+                Fun = fun(Field, Value) ->
+                    serialise(Tag, Field, Value, Opts)
+                end,
+                Proplist = lists:zipwith(Fun, Fields, Values),
+                {atom_to_binary(Tag, utf8), Proplist};
+            _ ->
+                erlang:error(badarg)
+        end,
     WrapFun(Term0).
 
 serialise(dns_message, Field, Datas, Opts) when is_list(Datas) ->
     Field0 = atom_to_binary(Field, utf8),
-    Datas0 = [ serialise(D, Opts) || D <- Datas ],
+    Datas0 = [serialise(D, Opts) || D <- Datas],
     {Field0, Datas0};
 serialise(dns_rr, data, Data, Opts) ->
     {<<"data">>, serialise(Data, Opts)};
-serialise(Tag, ip, Tuple, _Opts)
-  when Tag =:= dns_rrdata_a orelse Tag =:= dns_rrdata_aaaa ->
+serialise(Tag, ip, Tuple, _Opts) when Tag =:= dns_rrdata_a orelse Tag =:= dns_rrdata_aaaa ->
     {<<"ip">>, list_to_binary(inet_parse:ntoa(Tuple))};
 serialise(dns_rrdata_cert, cert, CRL, _Opts) ->
     {<<"cert">>, base64:encode(CRL)};
@@ -62,14 +63,17 @@ serialise(dns_rrdata_dlv, Field, Value, Opts) ->
     serialise(dns_rrdata_ds, Field, Value, Opts);
 serialise(dns_rrdata_key, public_key, PublicKey, _Opts) ->
     {<<"public_key">>, base64:encode(iolist_to_binary(PublicKey))};
-serialise(dns_rrdata_dnskey, public_key, PublicKey, _Opts)
-  when is_binary(PublicKey) -> {<<"public_key">>, base64:encode(PublicKey)};
+serialise(dns_rrdata_dnskey, public_key, PublicKey, _Opts) when is_binary(PublicKey) ->
+    {<<"public_key">>, base64:encode(PublicKey)};
 serialise(dns_rrdata_dnskey, public_key, PublicKey, _Opts) ->
-    PKMpint = [ begin
-                    BI = binary:encode_unsigned(I),
-                    L = byte_size(BI),
-                    <<L:32, BI/binary>>
-                end || I <- PublicKey ],
+    PKMpint = [
+        begin
+            BI = binary:encode_unsigned(I),
+            L = byte_size(BI),
+            <<L:32, BI/binary>>
+        end
+     || I <- PublicKey
+    ],
     {<<"public_key">>, base64:encode(iolist_to_binary(PKMpint))};
 serialise(dns_rrdata_ds, digest, Digest, _Opts) ->
     {<<"digest">>, bin_to_hex(Digest)};
@@ -77,15 +81,17 @@ serialise(dns_rrdata_ipseckey, gateway, Tuple, _Opts) when is_tuple(Tuple) ->
     {<<"gateway">>, list_to_binary(inet_parse:ntoa(Tuple))};
 serialise(dns_rrdata_ipseckey, public_key, PublicKey, _Opts) ->
     {<<"public_key">>, base64:encode(PublicKey)};
-serialise(Tag, salt, Salt, _Opts)
-  when Tag =:= dns_rrdata_nsec3 orelse Tag =:= dns_rrdata_nsec3param ->
-    Salt0 = case Salt of
-		<<>> -> <<$->>;
-		Salt -> bin_to_hex(Salt)
-	    end,
+serialise(Tag, salt, Salt, _Opts) when
+    Tag =:= dns_rrdata_nsec3 orelse Tag =:= dns_rrdata_nsec3param
+->
+    Salt0 =
+        case Salt of
+            <<>> -> <<$->>;
+            Salt -> bin_to_hex(Salt)
+        end,
     {<<"salt">>, Salt0};
 serialise(dns_rrdata_nsec3, hash, Hash, _Opts) ->
-    {<<"hash">>, base32:encode(Hash,[hex])};
+    {<<"hash">>, base32:encode(Hash, [hex])};
 serialise(dns_rrdata_rrsig, signature, Sig, _Opts) ->
     {<<"signature">>, base64:encode(Sig)};
 serialise(dns_rrdata_sshfp, fp, FP, _Opts) ->
@@ -95,7 +101,7 @@ serialise(dns_rrdata_tsig, mac, MAC, _Opts) ->
 serialise(dns_rrdata_tsig, other, Other, _Opts) ->
     {<<"other">>, bin_to_hex(Other)};
 serialise(dns_optrr, data, Datas, Opts) when is_list(Datas) ->
-    {<<"data">>, [ serialise(D, Opts) || D <- Datas ]};
+    {<<"data">>, [serialise(D, Opts) || D <- Datas]};
 serialise(dns_opt_nsid, data, Data, _Opts) when is_binary(Data) ->
     {<<"data">>, bin_to_hex(Data)};
 serialise(dns_opt_owner, Field, Data, _Opts) when is_binary(Data) ->
@@ -104,23 +110,24 @@ serialise(dns_opt_ecs, Field, Data, _Opts) when is_binary(Data) ->
     {atom_to_binary(Field, utf8), bin_to_hex(Data)};
 serialise(dns_opt_unknown, bin, Bin, _Opts) when is_binary(Bin) ->
     {<<"bin">>, bin_to_hex(Bin)};
-serialise(_Tag, Field, Value, _Opts) -> {atom_to_binary(Field, utf8), Value}.
+serialise(_Tag, Field, Value, _Opts) ->
+    {atom_to_binary(Field, utf8), Value}.
 
 deserialise(Term) -> deserialise(Term, []).
 
 deserialise(Term, Opts) ->
     UnwrapFun = proplists:get_value(wrap_fun, Opts, fun(X) -> X end),
     case UnwrapFun(Term) of
-	Bin when is_binary(Bin) -> hex_to_bin(Bin);
-	{TagBin, Props} when is_binary(TagBin) andalso is_list(Props) ->
-	    Tag = binary_to_existing_atom(TagBin, utf8),
-	    Fun = fun(Field) ->
-			  FieldBin = atom_to_binary(Field, utf8),
-			  Value = proplists:get_value(FieldBin, Props),
-			  deserialise(Tag, Field, Value, Opts)
-		  end,
-	    Values = [Fun(Field) || Field <- dns_record_info:fields(Tag) ],
-	    list_to_tuple([Tag|Values])
+        Bin when is_binary(Bin) -> hex_to_bin(Bin);
+        {TagBin, Props} when is_binary(TagBin) andalso is_list(Props) ->
+            Tag = binary_to_existing_atom(TagBin, utf8),
+            Fun = fun(Field) ->
+                FieldBin = atom_to_binary(Field, utf8),
+                Value = proplists:get_value(FieldBin, Props),
+                deserialise(Tag, Field, Value, Opts)
+            end,
+            Values = [Fun(Field) || Field <- dns_record_info:fields(Tag)],
+            list_to_tuple([Tag | Values])
     end.
 
 deserialise_dnskey_publickey(PublicKeyB64) ->
@@ -130,14 +137,17 @@ deserialise_dnskey_publickey(PublicKeyB64) ->
 deserialise_dnskey_publickey(_PK, <<>>, Ints) ->
     lists:reverse(Ints);
 deserialise_dnskey_publickey(PK, <<L:32, I:L/unit:8, Rest/binary>>, Ints) ->
-    deserialise_dnskey_publickey(PK, Rest, [I|Ints]);
-deserialise_dnskey_publickey(PK, _, _) -> PK.
+    deserialise_dnskey_publickey(PK, Rest, [I | Ints]);
+deserialise_dnskey_publickey(PK, _, _) ->
+    PK.
 
 deserialise(dns_message, _Field, Terms, Opts) when is_list(Terms) ->
-    [ deserialise(Term, Opts) || Term <- Terms ];
-deserialise(dns_rr, data, Term, Opts) -> deserialise(Term, Opts);
-deserialise(Tag, ip, IP, _Opts)
-  when Tag =:= dns_rrdata_a orelse Tag =:= dns_rrdata_aaaa ->
+    [deserialise(Term, Opts) || Term <- Terms];
+deserialise(dns_rr, data, Term, Opts) ->
+    deserialise(Term, Opts);
+deserialise(Tag, ip, IP, _Opts) when
+    Tag =:= dns_rrdata_a orelse Tag =:= dns_rrdata_aaaa
+->
     {ok, Tuple} = inet_parse:address(binary_to_list(IP)),
     Tuple;
 deserialise(dns_rrdata_cert, cert, CRL, _Opts) ->
@@ -154,13 +164,14 @@ deserialise(dns_rrdata_ds, digest, Digest, _Opts) ->
     hex_to_bin(Digest);
 deserialise(dns_rrdata_ipseckey, gateway, Gateway, _Opts) ->
     case inet_parse:address(binary_to_list(Gateway)) of
-	{ok, Tuple} -> Tuple;
-	{error, einval} -> Gateway
+        {ok, Tuple} -> Tuple;
+        {error, einval} -> Gateway
     end;
 deserialise(dns_rrdata_ipseckey, public_key, PublicKey, _Opts) ->
     base64:decode(PublicKey);
-deserialise(Tag, salt, Salt, _Opts)
-  when Tag =:= dns_rrdata_nsec3 orelse Tag =:= dns_rrdata_nsec3param ->
+deserialise(Tag, salt, Salt, _Opts) when
+    Tag =:= dns_rrdata_nsec3 orelse Tag =:= dns_rrdata_nsec3param
+->
     hex_to_bin(Salt);
 deserialise(dns_rrdata_nsec3, hash, Hash, _Opts) ->
     base32:decode(Hash, [hex]);
@@ -173,7 +184,7 @@ deserialise(dns_rrdata_tsig, mac, MAC, _Opts) ->
 deserialise(dns_rrdata_tsig, other, Other, _Opts) ->
     hex_to_bin(Other);
 deserialise(dns_optrr, data, Terms, Opts) ->
-    [ deserialise(Term, Opts) || Term <- Terms ];
+    [deserialise(Term, Opts) || Term <- Terms];
 deserialise(dns_opt_nsid, data, Data, _Opts) when is_binary(Data) ->
     hex_to_bin(Data);
 deserialise(dns_opt_owner, _Field, Data, _Opts) when is_binary(Data) ->
@@ -182,55 +193,69 @@ deserialise(dns_opt_ecs, _Field, Data, _Opts) when is_binary(Data) ->
     hex_to_bin(Data);
 deserialise(dns_opt_unknown, bin, Bin, _Opts) when is_binary(Bin) ->
     hex_to_bin(Bin);
-deserialise(_Tag, _Field, Value, _Opts) -> Value.
+deserialise(_Tag, _Field, Value, _Opts) ->
+    Value.
 
 %% Internal
 
 bin_to_hex(Bin) when is_binary(Bin) ->
-    iolist_to_binary([ io_lib:format("~2.16.0B", [V]) || <<V>> <= Bin ]).
+    iolist_to_binary([io_lib:format("~2.16.0B", [V]) || <<V>> <= Bin]).
 
 hex_to_bin(Bin) when is_binary(Bin) ->
     Fun = fun(A, B) ->
-	     case io_lib:fread("~16u", [A, B]) of
-		 {ok, [V], []} -> V;
-		 _ -> error(badarg)
-	     end
-	  end,
-    << <<(Fun(A,B))>> || <<A, B>> <= Bin >>.
+        case io_lib:fread("~16u", [A, B]) of
+            {ok, [V], []} -> V;
+            _ -> error(badarg)
+        end
+    end,
+    <<<<(Fun(A, B))>> || <<A, B>> <= Bin>>.
 
 -ifdef(TEST).
 
 dnssec_cases() ->
     {ok, Cases} = file:consult(filename:join(prefix(), "dnssec_samples.txt")),
-    [ {ZoneName, dns:decode_message(Bin)}
-      || {ZoneName, _RawKeys, Bin} <- Cases ].
+    [
+        {ZoneName, dns:decode_message(Bin)}
+     || {ZoneName, _RawKeys, Bin} <- Cases
+    ].
 
 optrr_cases() ->
-    [ {atom_to_list(element(1, Opt)),
-       #dns_message{additional = [#dns_optrr{data = [Opt]}]}}
-      || Opt <- [#dns_opt_llq{opcode = ?DNS_LLQOPCODE_SETUP,
-			      errorcode = ?DNS_LLQERRCODE_NOERROR,
-			      id = dns:random_id(),
-			      leaselife = 42},
-		 #dns_opt_nsid{data = <<222,175>>},
-		 #dns_opt_owner{seq = 0,
-				primary_mac = <<17,28,15,254,225,17>>,
-				_ = <<>>},
-		 #dns_opt_ul{lease = 42},
-		 #dns_opt_unknown{id = 1, bin = <<222,173,192,222>>}] ].
+    [
+        {atom_to_list(element(1, Opt)), #dns_message{additional = [#dns_optrr{data = [Opt]}]}}
+     || Opt <- [
+            #dns_opt_llq{
+                opcode = ?DNS_LLQOPCODE_SETUP,
+                errorcode = ?DNS_LLQERRCODE_NOERROR,
+                id = dns:random_id(),
+                leaselife = 42
+            },
+            #dns_opt_nsid{data = <<222, 175>>},
+            #dns_opt_owner{
+                seq = 0,
+                primary_mac = <<17, 28, 15, 254, 225, 17>>,
+                _ = <<>>
+            },
+            #dns_opt_ul{lease = 42},
+            #dns_opt_unknown{id = 1, bin = <<222, 173, 192, 222>>}
+        ]
+    ].
 
 rrdata_cases() ->
     {ok, Cases} = file:consult(filename:join(prefix(), "rrdata_wire_samples.txt")),
-    [ {lists:flatten(io_lib:format("~p/~p", [Class, Type])),
-       dns:decode_rrdata(Class, Type, Bin)} || {Class, Type, Bin} <- Cases ].
+    [
+        {lists:flatten(io_lib:format("~p/~p", [Class, Type])), dns:decode_rrdata(Class, Type, Bin)}
+     || {Class, Type, Bin} <- Cases
+    ].
 
 tsig_cases() ->
     {ok, Cases} = file:consult(filename:join(prefix(), "tsig_wire_samples.txt")),
-    [ {Name, dns:decode_message(Msg)} || {Name, Msg} <- Cases ].
+    [{Name, dns:decode_message(Msg)} || {Name, Msg} <- Cases].
 
 tests(Cases, Serialise, Deserialise) ->
-    [ {TestName, ?_assertEqual(Term, Deserialise(Serialise(Term)))}
-      || {TestName, Term} <- Cases ].
+    [
+        {TestName, ?_assertEqual(Term, Deserialise(Serialise(Term)))}
+     || {TestName, Term} <- Cases
+    ].
 
 erl_tests(Cases) -> tests(Cases, fun serialise/1, fun deserialise/1).
 
@@ -243,23 +268,27 @@ serialise_rrdata_test_() -> erl_tests(rrdata_cases()).
 serialise_tsig_test_() -> erl_tests(tsig_cases()).
 
 ejson_serialise(Term) ->
-    Fun = fun({K,V}) -> {[{<<"_tag">>, K}|V]};
-	     (Bin) when is_binary(Bin) -> Bin end,
+    Fun = fun
+        ({K, V}) -> {[{<<"_tag">>, K} | V]};
+        (Bin) when is_binary(Bin) -> Bin
+    end,
     ejson:encode(serialise(Term, [{wrap_fun, Fun}])).
 
 ejson_deserialise(Term) ->
-    Fun = fun({V}) when is_list(V) ->
-		  {proplists:get_value(<<"_tag">>, V), V};
-	     (Term0) -> Term0
-	  end,
+    Fun = fun
+        ({V}) when is_list(V) ->
+            {proplists:get_value(<<"_tag">>, V), V};
+        (Term0) ->
+            Term0
+    end,
     deserialise(ejson:decode(Term), [{wrap_fun, Fun}]).
 
 json_tests(Cases) ->
     Serialise = fun ejson_serialise/1,
     Deserialise = fun ejson_deserialise/1,
     case code:ensure_loaded(ejson) of
-	{module, ejson} -> tests(Cases, Serialise, Deserialise);
-	{error, nofile} -> []
+        {module, ejson} -> tests(Cases, Serialise, Deserialise);
+        {error, nofile} -> []
     end.
 
 json_dnssec_test_() -> json_tests(dnssec_cases()).
