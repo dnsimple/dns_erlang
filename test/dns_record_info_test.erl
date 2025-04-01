02aa19417513d80868a1836bb2bafef8d1bd6a35
Nelson Vides <nelson.vides@erlang-solutions.com>
2025-04-01 10:23:00 +0200

    Clear reserved priv dir and move txt test helpers into tests

--- test/dns_record_info_test.erl
+++ test/dns_record_info_test.erl
@@ -3,7 +3,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 type_rec_test_() ->
-    {ok, Cases} = file:consult(filename:join("priv", "rrdata_wire_samples.txt")),
+    {ok, Cases} = file:consult(filename:join("test", "rrdata_wire_samples.txt")),
     Types = sets:to_list(sets:from_list([T || {_, T, _} <- Cases, T =/= 999])),
     [
         ?_assertEqual(Type, dns_record_info:type_for_atom(dns_record_info:atom_for_type(Type)))
@@ -11,7 +11,7 @@ type_rec_test_() ->
     ].
 
 recinfo_test_() ->
-    {ok, Cases} = file:consult(filename:join("priv", "rrdata_wire_samples.txt")),
+    {ok, Cases} = file:consult(filename:join("test", "rrdata_wire_samples.txt")),
     Types = sets:to_list(sets:from_list([T || {_, T, _} <- Cases, T =/= 999])),
     Tags = [dns_rr | [dns_record_info:atom_for_type(Type) || Type <- Types]],
     [
